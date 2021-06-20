ce2546cc0f00d59d731d5f52d4336da15e1cf4fd
victorsoto2021 <85852208+victorsoto2021@users.noreply.github.com>
2021-06-20 04:32:15 -0500

    Add files via upload

---
+++ JS/funcion.js
@@ -0,0 +1,16 @@
+
+function PP() {
+    alert("HAS VOTADO POR PARTIDO POPULAR");
+}
+
+function CS() {
+    alert("HAS VOTADO POR PARTIDO CIUDADANOS");
+}
+
+function PSOE() {
+    alert("HAS VOTADO POR PARTIDO SOCIALISTA OBRERO ESPAÑOL");
+}
+
+function PODEMOS() {
+    alert("HAS VOTADO POR PODEMOS");
+}
